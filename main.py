e973ca135f62fa6a48d2b2bf2fb4a38431b63b2e
KornelSzyszka <kszyszka13@gmail.com>
2023-11-18 22:22:54 +0100

    added main file

---
+++ main.py
@@ -0,0 +1,19 @@
+import pygame
+
+# pygame setup
+pygame.init()
+screen = pygame.display.set_mode((1280, 720))
+clock = pygame.time.Clock()
+running = True
+
+while running:
+    # pygame.QUIT event means the user clicked X to close your window
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    pygame.display.flip()
+
+    clock.tick(60)  # limits FPS to 60
+
+pygame.quit()
