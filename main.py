c066a36ba82ef80d88169d38791e54d07a1cec2a
KornelSzyszka <kszyszka13@gmail.com>
2023-11-18 23:14:30 +0100

    GFX and main modifications

--- main.py
+++ main.py
@@ -1,19 +1,37 @@
 import pygame
+import sys
 
-# pygame setup
 pygame.init()
-screen = pygame.display.set_mode((1280, 720))
+
+WIDTH, HEIGHT, FPS_CAP = 1280, 720, 60
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
 clock = pygame.time.Clock()
-running = True
 
-while running:
-    # pygame.QUIT event means the user clicked X to close your window
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+pygame.display.set_caption("Space Shooter")
 
-    pygame.display.flip()
+background = pygame.image.load("GFX/background.png")
+spaceship = pygame.image.load("GFX/spaceship.png")
 
-    clock.tick(60)  # limits FPS to 60
+SPACESHIP_WIDTH, SPACESHIP_HEIGHT = 50, 40
 
-pygame.quit()
+def main():
+    running = True
+
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        screen.blit(background, (0, 0))
+        screen.blit(spaceship, (WIDTH/2 - SPACESHIP_HEIGHT/2, HEIGHT/2 - SPACESHIP_WIDTH/2))
+
+        pygame.display.flip()
+
+        clock.tick(FPS_CAP)
+
+    pygame.quit()
+    sys.exit()
+
+
+if __name__ == '__main__':
+    main()
