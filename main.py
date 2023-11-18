ce6f93c75cf2df0b6af3946bd3adc3af66009647
Marcin267 <marcin210203@gmail.com>
2023-11-19 00:22:59 +0100

    spaceship movement

--- main.py
+++ main.py
@@ -9,10 +9,12 @@ clock = pygame.time.Clock()
 
 pygame.display.set_caption("Space Shooter")
 
-background = pygame.image.load("GFX/background.png")
-spaceship = pygame.image.load("GFX/spaceship.png")
+background_image = pygame.image.load("GFX/background.png")
+spaceship_image = pygame.image.load("GFX/spaceship.png")
 
 SPACESHIP_WIDTH, SPACESHIP_HEIGHT = 50, 40
+SPACE_SHIP_SPEED = 5
+
 
 def main():
     running = True
@@ -22,8 +24,22 @@ def main():
             if event.type == pygame.QUIT:
                 running = False
 
-        screen.blit(background, (0, 0))
-        screen.blit(spaceship, (WIDTH/2 - SPACESHIP_HEIGHT/2, HEIGHT/2 - SPACESHIP_WIDTH/2))
+        screen.blit(background_image, (0, 0))
+
+        spaceship_pos_x, spaceship_pos_y = (WIDTH - SPACESHIP_WIDTH) // 2, (HEIGHT - SPACESHIP_HEIGHT) // 2
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT] and spaceship_pos_x > 0:
+            spaceship_pos_x -= SPACE_SHIP_SPEED
+        if keys[pygame.K_RIGHT] and spaceship_pos_x < WIDTH - SPACESHIP_WIDTH:
+            spaceship_pos_x += SPACE_SHIP_SPEED
+        if keys[pygame.K_UP] and spaceship_pos_y > 0:
+            spaceship_pos_y -= SPACE_SHIP_SPEED
+        if keys[pygame.K_DOWN] and spaceship_pos_y < HEIGHT - SPACESHIP_HEIGHT:
+            spaceship_pos_y += SPACE_SHIP_SPEED
+
+        spaceship_rect_surface = pygame.Surface((SPACESHIP_WIDTH, SPACESHIP_HEIGHT), pygame.SRCALPHA)
+        spaceship_rect_surface.blit(spaceship_image, (0, 0))
+        screen.blit(spaceship_rect_surface, (spaceship_pos_x, spaceship_pos_y))
 
         pygame.display.flip()
 
